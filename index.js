4f8e075627725da70e6c7ae253407f19d3981e80
Mercede Chen <mercedechen@gmail.com>
2023-11-19 18:19:44 -0500

    Create object containing an array of available freelancers

---
+++ index.js
@@ -0,0 +1,12 @@
+const freelancers = [
+  {name: "Alice", occupation: "Writer", price: 30},
+  {name: "Bob", occupation: "Teacher", price: 50},
+  {name: "Carol", occupation: "Programmer", price: 70},
+  {name: "Emily", occupation: "Artist", price: 100},
+  {name: "Rebecca", occupation: "Graphic Designer", price: 75},
+  {name: "George", occupation: "Teacher", price: 40},
+  {name: "Kim", occupation: "Reporter", price: 35},
+  {name: "Aaron", occupation: "Software Developer", price: 100}
+];
+
+console.log(freelancers);
