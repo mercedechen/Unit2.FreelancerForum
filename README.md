ec808d0f98ac561d0b88279b699259118f6b503c
Mercede Chen <mercedechen@gmail.com>
2023-11-20 09:00:48 -0500

    Update README, remove a file, create all elements using JS

--- README.md
+++ README.md
@@ -7,3 +7,25 @@ The visitor also finds a message that displays the average starting price of all
 A few seconds later, a new listing appears for a freelancer named Carol, who is a programmer and has a starting price of $70. The average starting price is updated to $50.
 
 New freelancers continue to appear every few seconds, and the average starting price is updated accordingly.
+
+## Notes and Approach
+
+1. Create HTML file with connected CSS and JS files.
+
+2. Create "Freelancer Forum" in h1 tag, "Available Freelancers" in h2 tag and "The average starting price is:" in p tag.
+
+3. Create an array containing the list of freelancers.
+Example:
+  {name: "Alice", occupation: "Writer", price: 30},
+  {name: "Bob", occupation: "Teacher", price: 50}
+
+4. Create an unordered list of the available freelancers.
+
+5. Calculate the starting average price of the freelancers.
+
+6. Using setInterval(), add additional freelancers at a specified interval and render() every few seconds to the unordered list.
+
+7. Update the average starting price.
+
+
+
--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-// Adds h1 title to document using JavaScript
-const title = document.createElement('h1');
-title.textContent = "Freelancer Forum";
-document.body.appendChild(title);
+// Create h1 tag
+const h1 = document.createElement("h1");
+h1.textContent = "Freelancer Forum";
+document.body.append(h1);
 
-// Adds h2 title to document using JavaScript
-const section = document.createElement('h2');
-section.textContent = "Available Freelancers";
-document.body.appendChild(section);
+// Create h2 tag
+const h2 = document.createElement("h2");
+h2.textContent = "Available Freelancers";
+document.body.append(h2);
 
 const freelancers = [
   {name: "Alice", occupation: "Writer", price: 30},
   {name: "Bob", occupation: "Teacher", price: 50},
 ];
 
-const otherFreelancers = [
-  {name: "Carol", occupation: "Programmer", price: 70}
-  {name: "Ruby", occupation: "Graphic Designer", price: 100}
-]
+// Create an unordered list
+let ul = document.createElement("ul");
+document.body.append(ul);
 
-const ul = document.querySelector(#freelancersList);
+// Add list items to unordered list
+const freelancersList = freelancers.map((freelancer) => {
+  const li = document.createElement("li");
+  li.textContent = `Name: ${freelancer.name}, Occupation: ${freelancer.occupation}, Price: ${freelancer.price}`;
+  ul.appendChild(li);
+});
 
+// Create p tag
+let p = document.createElement("p");
+p.textContent = "The starting average price is: "
+document.body.append(p);
 
-
-
+// Calculate the starting average price of available freelancers
 
