02b885443a6c7d773f4c584f1cc4cf37be815087
Mercede Chen <mercedechen@gmail.com>
2023-11-20 14:15:11 -0500

    Add new available freelancers to the list at a set interval

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-// Create h1 tag
+// Create headings
 const h1 = document.createElement("h1");
 h1.textContent = "Freelancer Forum";
 document.body.append(h1);
 
-// Create h2 tag
 const h2 = document.createElement("h2");
 h2.textContent = "Available Freelancers";
 document.body.append(h2);
 
+// Create array containing available freelancers
 const freelancers = [
   {name: "Alice", occupation: "Writer", price: 30},
   {name: "Bob", occupation: "Teacher", price: 50},
 ];
 
-const newFreelancers = [
-  {name: "Carol", occupation: "Programmer", price: 70},
-  {name: "Emily", occupation: "Reporter", price: 45},
-  {name: "Alan", occupation: "Developer", price: 100}
-]
-
 // Create an unordered list
 let ul = document.createElement("ul");
 document.body.append(ul);
 
-// Add available freelancers to unordered list
+// Create an unordered list and add initial available freelancers
 const freelancersList = freelancers.map((freelancer) => {
   const li = document.createElement("li");
   li.textContent = `Name: ${freelancer.name}, Occupation: ${freelancer.occupation}, Price: ${freelancer.price}`;
@@ -39,22 +33,32 @@ function calculateAveragePrice(freelancers) {
   let startingAveragePrice = sum / freelancers.length;
   return startingAveragePrice;
 }
-
 startingAveragePrice = calculateAveragePrice(freelancers);
-// console.log(startingAveragePrice);
 
-// Create p tag
+// Display the starting average price on HTML
 let p = document.createElement("p");
 p.textContent = `The starting average price is: ${startingAveragePrice}`;
 document.body.append(p);
 
 // Add new available freelancers to unordered list
-// setInterval();
-function addFreelancers() {
-  const newFreelancersList = newFreelancers.map((newFreelancer) => {
+function addFreelancers(newFreelancers) {
+  newFreelancers.map((newFreelancer) => {
     const li = document.createElement("li");
     li.textContent = `Name: ${newFreelancer.name}, Occupation: ${newFreelancer.occupation}, Price: ${newFreelancer.price}`;
     ul.appendChild(li);
   });
 };
-addFreelancers(newFreelancers);
+// addFreelancers(newFreelancers);
+
+// Add new available freelancers at a set interval
+const addFreelancersInterval = setInterval(() => {
+  const newFreelancers = [
+    {name: "Carol", occupation: "Programmer", price: 70},
+    {name: "Dave", occupation: "Reporter", price: 45}
+  ];
+  addFreelancers(newFreelancers);
+
+//   if (freelancers.length >= 5) {
+//     clearInterval(addFreelancersInterval);
+//   };
+// }, 1000);
